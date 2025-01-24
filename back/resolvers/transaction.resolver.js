3481d5c39a46c52f524d082120427cbf0dbbf6a1
usykDev <marishaa20@gmail.com>
2025-01-24 14:12:26 +0200

    transaction resolvers added

--- back/resolvers/transaction.resolver.js
+++ back/resolvers/transaction.resolver.js
@@ -1,6 +1,106 @@
+import Transaction from "../models/transaction.model.js";
+
 const transactionResolver = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    transactions: async (_, __, context) => {
+      try {
+        if (!context.getUser()) throw new Error("Unauthorized");
+
+        const userId = await context.getUser()._id;
+
+        const transactions = Transaction.find({ userId });
+        return transactions;
+      } catch (error) {
+        console.error("Error gettion transactions: ", error);
+        throw new Error(error.message || "Error gettion transactions");
+      }
+    },
+
+    transaction: async (_, { transactionId }) => {
+      try {
+        const transaction = await Transaction.findById(transactionId);
+        return transaction;
+      } catch (error) {
+        console.error("Error gettion transaction: ", error);
+        throw new Error(error.message || "Error gettion transaction");
+      }
+    },
+
+    // todo => add categotyStatistics query
+  },
+  Mutation: {
+    createTransaction: async (_, { input }, context) => {
+      try {
+        // const { description, paymentType, category, amount, location, date } =
+        //   input;
+
+        // if (!description || !paymentType || !category || !amount || !date) {
+        //   throw new Error("Please fill in all required fields");
+        // }
+
+        const newTransaction = new Transaction({
+          //   userId,
+          //   description,
+          //   paymentType,
+          //   category,
+          //   amount,
+          //   location,
+          //   date,
+          ...input,
+          userId: context.getUser()._id,
+        });
+
+        await newTransaction.save();
+
+        return newTransaction;
+      } catch (error) {
+        console.error("Error creating transaction: ", error);
+        throw new Error(error.message || "Error creating transaction");
+      }
+    },
+
+    updateTransaction: async (_, { input }) => {
+      try {
+        // const { description, paymentType, category, amount, location, date } =
+        //   input;
+
+        // if (!description || !paymentType || !category || !amount || !date) {
+        //   throw new Error("Please fill in all required fields");
+        // }
+
+        // const transaction = Transaction.findById(input.transactionId)
+
+        const updatedTransaction = await Transaction.findByIdAndUpdate(
+          input.transactionId,
+          input,
+          { new: true }
+        );
+        return updatedTransaction;
+      } catch (error) {
+        console.error("Error updating transaction: ", error);
+        throw new Error(error.message || "Error updating transaction");
+      }
+    },
+
+    deleteTransaction: async (_, { transactionId }, context) => {
+      try {
+        await Transaction.findByIdAndDelete(transactionId);
+
+        if (!context.getUser()) throw new Error("Unauthorized");
+
+        const userId = await context.getUser()._id;
+
+        const transactions = Transaction.find({ userId });
+
+        return transactions;
+      } catch (error) {
+        console.error("Error deleting transaction: ", error);
+        throw new Error(error.message || "Error deleting transaction");
+      }
+    },
+
+    // todo => add thansaction/user relationship
+  },
 };
 
 export default transactionResolver;
