251fce8723e6ec52d821317198399455fbc9e88a
usykDev <marishaa20@gmail.com>
2025-01-28 11:31:31 +0200

    ui first commit

---
+++ front/src/app/login/page.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { ChangeEvent, FormEvent, useState } from "react";
+import InputField from "@/components/ui/InputField";
+import Link from "next/link";
+
+const LoginPage = () => {
+  const [loginData, setLoginData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setLoginData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(loginData);
+  };
+
+  return (
+    <div className="flex justify-center items-center">
+      <div className="flex rounded-lg overflow-hidden z-50 bg-gray-300">
+        <div className="w-full bg-gray-100 min-w-64 sm:min-w-96 flex items-center justify-center">
+          <div className="max-w-md w-full p-4 sm:p-6">
+            <h1 className="text-3xl font-semibold mb-3 text-primary-dark text-center">
+              Login
+            </h1>
+            <h1 className="text-sm font-semibold mb-6 text-gray-500 text-center">
+              Welcome back! Log in to your account
+            </h1>
+            <form className="space-y-4" onSubmit={handleSubmit}>
+              <InputField
+                label="Username"
+                id="username"
+                name="username"
+                value={loginData.username}
+                onChange={handleChange}
+              />
+
+              <InputField
+                label="Password"
+                id="password"
+                name="password"
+                type="password"
+                value={loginData.password}
+                onChange={handleChange}
+              />
+              <div>
+                <button
+                  type="submit"
+                  className="w-full bg-primary-medium text-white p-2 rounded-md hover:bg-primary-dark focus:outline-none focus:bg-black  focus:ring-2 focus:ring-offset-2 focus:ring-gray-900 transition-colors duration-300 disabled:opacity-50 disabled:cursor-not-allowed"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+            <div className="mt-4 text-sm text-gray-600 text-center">
+              <p>
+                {"Don't"} have an account?{" "}
+                <Link
+                  href="/register"
+                  className="text-primary-dark font-bold hover:underline"
+                >
+                  Register
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default LoginPage;
---
+++ front/src/app/transaction/[id]/page.tsx
@@ -0,0 +1,18 @@
+import TransactionForm from "@/components/ui/TransactionForm";
+import TransactionFormSkeleton from "@/components/skeletons/TransactionFormSkeleton";
+
+const Transaction = () => {
+  //   return <TransactionFormSkeleton />;
+
+  return (
+    <div className=" flex flex-col justify-center items-center">
+      <p className="text-2xl font-bold text-center relative z-50 mb-4 bg-gradient-to-r from-secondary-300 via-pink-400 to-secondary-300 inline-block text-transparent bg-clip-text">
+        Update this transaction
+      </p>
+
+      <TransactionForm />
+    </div>
+  );
+};
+
+export default Transaction;
